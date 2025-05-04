ac8e6913909a6c164227ad029235ad04b474b94c
Fco Kauan <fcokauanclash@gmail.com>
2025-05-04 12:23:51 -0300

    criando componentes

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -2,7 +2,7 @@ const StartScreen = () => {
   return (
     <div>
         <h1 className="text-[3.5em] font-medium"> Palavra Secreta </h1>
-        <p className="mb-[2em] color text-yellow-300">Clique no botão abaixo para começar a jogar</p>
+        <p className="mb-[2em] color text-yellow-300 mt-[1.2em]">Clique no botão abaixo para começar a jogar</p>
         <button> Começar o jogo </button>
     </div>
   )
