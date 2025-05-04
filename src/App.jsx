56243b1cb07d72160bd98117fe1c38c7a17396a6
Fco Kauan <fcokauanclash@gmail.com>
2025-05-04 12:03:43 -0300

    finalizando tela inicial do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import StartScreen from './components/StartScreen'
 
 function App() {
 
   return (
     <div className='App'>
-      <h1 className='text-8xl'>Palavra Secreta</h1>
+      <StartScreen />
     </div>
   )
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,11 @@
+const StartScreen = () => {
+  return (
+    <div>
+        <h1 className="text-[3.5em] font-medium"> Palavra Secreta </h1>
+        <p className="mb-[2em] color text-yellow-300">Clique no botão abaixo para começar a jogar</p>
+        <button> Começar o jogo </button>
+    </div>
+  )
+}
+
+export default StartScreen
