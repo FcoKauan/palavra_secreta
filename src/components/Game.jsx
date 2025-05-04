93c42076df7cca29ea36591d8303852bf8b9ea8e
Fco Kauan <fcokauanclash@gmail.com>
2025-05-04 14:02:32 -0300

    estruturando componente de game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,32 @@
 const Game = ({verifyLetter}) => {
   return (
     <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>
-            Game OVER
-        </button>
+        <p>
+            <span>Pontuação: 000</span>
+        </p>
+        <h1>Adivinhe a palavra:</h1>
+        <h3>Dica sobre a palavra: <span>Dica...</span></h3>
+        {/* word container */}
+        <div>
+            {/* letra */}
+            <span>A</span>
+            {/* quadrado branco */}
+            <span></span>
+        </div>
+        {/* letter container */}
+        <div>   
+            <p>Tente adivinhar uma letra da palavra: </p>
+            <form>
+                <input className="bg-white" type="text" name="letter" maxLength="1" required/>
+                <button> Jogar! </button>
+            </form>
+        </div>
+        {/*  wrong letters container*/}
+        <div>
+            <p>Letras já utilizadas:</p>
+            <span>a,</span>
+            <span>b,</span>
+        </div>
     </div>
   )
 }
